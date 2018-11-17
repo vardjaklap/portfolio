22b578680cb7f8374af22f00de17c70d0ae36a51
vardjaklap <vardjaklap@gmail.com>
2018-11-17 14:06:35 -0500

    Updates

---
+++ precache-manifest.89f56ff3f6c38d2ee25a22755081ed93.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "63a88e1317b593f89332",
+    "url": "/portfolio/static/css/main.19c648a9.chunk.css"
+  },
+  {
+    "revision": "63a88e1317b593f89332",
+    "url": "/portfolio/static/js/main.63a88e13.chunk.js"
+  },
+  {
+    "revision": "5bd59433943ccd176ab5",
+    "url": "/portfolio/static/js/1.5bd59433.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4d375a6b8162227b94bd3d756f7ab4e6",
+    "url": "/portfolio/static/media/photo1.4d375a6b.png"
+  },
+  {
+    "revision": "c09756930e11a3d052f778a0e6ab88bc",
+    "url": "/portfolio/static/media/photo2.c0975693.png"
+  },
+  {
+    "revision": "2948b3ca123ccfc3084722e96ff41cbc",
+    "url": "/portfolio/static/media/photo3.2948b3ca.png"
+  },
+  {
+    "revision": "eebe87787da29ec2e3176029a2283b8c",
+    "url": "/portfolio/static/media/photo4.eebe8778.png"
+  },
+  {
+    "revision": "ffaaa5fc4d794afd4c5449890de94455",
+    "url": "/portfolio/static/media/photo5.ffaaa5fc.png"
+  },
+  {
+    "revision": "b561734af281a715d5b4c7eb51b32ec5",
+    "url": "/portfolio/static/media/photo6.b561734a.png"
+  },
+  {
+    "revision": "b5a6d0d5ae1a6a876ef62cb135d98c4f",
+    "url": "/portfolio/static/media/photo7.b5a6d0d5.png"
+  },
+  {
+    "revision": "71549dbae69539ff0a0fc0e95fcfe0ba",
+    "url": "/portfolio/static/media/photo8.71549dba.png"
+  },
+  {
+    "revision": "be23954a8238ea4ce7fcbedb0395607e",
+    "url": "/portfolio/static/media/photo9.be23954a.png"
+  },
+  {
+    "revision": "86b4c231385ec5d7c29d658973215f84",
+    "url": "/portfolio/static/media/avatar.86b4c231.jpg"
+  },
+  {
+    "revision": "651ff0eeb9b74589eca9b3b0c7ef51ef",
+    "url": "/portfolio/static/media/headergb.651ff0ee.jpg"
+  },
+  {
+    "revision": "77bba11ed84c2ea631ebe73a027a10d9",
+    "url": "/portfolio/index.html"
+  }
+];
